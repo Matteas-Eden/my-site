f5b6d2d9a5aadcd05b3371d2cfafba87debbbf92
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2022-03-25 00:11:16 +1300

    Introduce the 'Accordion' component
    Taking this from the Semantic/Formantic UI library, this component
    is included to try and improve the dropdowns for different projects.

--- src/articles/Work.tsx
+++ src/articles/Work.tsx
@@ -1,40 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ArticleProps } from './ArticleProps';
 import { Outerstellar } from './work/Outerstellar';
+import { Accordion, Icon } from 'semantic-ui-react';
 
-export const Work = ({ className, close }: ArticleProps) => (
-    <article
-        id="work"
-        className={className}
-        style={{ display: 'none' }}
-    >
-        <h2 className="major">Work</h2>
-        {/* <details>
-            <summary>Roll for Reaction [2020]</summary>
-            <i>Outerstellar</i> is a top-down arcade space shooter made in
-            GameMaker. The story goes that a group of high-schoolers used a
-            government grant to fund an "Arcade Games Hackathon", my computer
-            science teacher at the time told our class about it, and then I
-            signed up. Queue me being paired...
-        </details>
-        <details>
-            <summary>Energize [2019]</summary>
-            <i>Outerstellar</i> is a top-down arcade space shooter made in
-            GameMaker. The story goes that a group of high-schoolers used a
-            government grant to fund an "Arcade Games Hackathon", my computer
-            science teacher at the time told our class about it, and then I
-            signed up. Queue me being paired...
-        </details>
-        <details>
-            <summary>Wireless Energy Monitor [2018]</summary>
-            <i>Outerstellar</i> is a top-down arcade space shooter made in
-            GameMaker. The story goes that a group of high-schoolers used a
-            government grant to fund an "Arcade Games Hackathon", my computer
-            science teacher at the time told our class about it, and then I
-            signed up. Queue me being paired...
-        </details> */}
-        <Outerstellar />
+import '../assets/css/semantic-accordion.css';
 
-        {close}
-    </article >
-)
+export const Work = ({ className, close }: ArticleProps) => {
+
+    const [activeIndex, setActiveIndex] = useState<number>(-1);
+
+    const updateActive = (index: number) => {
+        if (index === activeIndex) {
+            setActiveIndex(-1);
+        } else {
+            setActiveIndex(index);
+        }
+    }
+
+    /** Refactoring notes
+     * Iterate over several components
+     * and dynamically allocate index
+     * 
+     * also maybe divide into 'past' and 'current' projects
+     * 
+     * also look into callbacks for making the accordion items modular
+     * but still changing the state
+     */
+    return (
+        <article
+            id="work"
+            className={className}
+            style={{ display: 'none' }}
+        >
+            <h2 className="major">Work</h2>
+            <Accordion inverted>
+                <Accordion.Title
+                 active={activeIndex === 0}
+                 index={0}
+                 onClick={() => updateActive(0)}
+                >
+                    Roll for Reaction [2020]
+                </Accordion.Title>
+                <Accordion.Content active={activeIndex === 0}>
+                    Hello world
+                </Accordion.Content>
+                <Accordion.Title
+                 active={activeIndex === 1}
+                 index={1}
+                 onClick={() => updateActive(1)}
+                >
+                    Energize [2019]
+                </Accordion.Title>
+                <Accordion.Content active={activeIndex === 1}>
+                    <i>Outerstellar</i> is a top-down arcade space shooter made in
+                    GameMaker. The story goes that a group of high-schoolers used a
+                    government grant to fund an "Arcade Games Hackathon", my computer
+                    science teacher at the time told our class about it, and then I
+                    signed up. Queue me being paired...
+                </Accordion.Content>
+                <Accordion.Title
+                 active={activeIndex === 2}
+                 index={2}
+                 onClick={() => updateActive(2)}
+                >
+                    Wireless Energy Monitor [2018]
+                </Accordion.Title>
+                <Accordion.Content active={activeIndex === 2}>
+                    <i>Outerstellar</i> is a top-down arcade space shooter made in
+                    GameMaker. The story goes that a group of high-schoolers used a
+                    government grant to fund an "Arcade Games Hackathon", my computer
+                    science teacher at the time told our class about it, and then I
+                    signed up. Queue me being paired...
+                </Accordion.Content>
+                {/* <Outerstellar /> */}
+            </Accordion>
+
+
+            {close}
+        </article >)
+}
