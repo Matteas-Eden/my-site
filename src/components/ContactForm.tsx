5fe6e72232fbe8da2c5414e8fbfda45d662e05dc
Matt Eden <45587386+Matteas-Eden@users.noreply.github.com>
2022-08-25 19:57:06 +1200

    Improve validation for contact form

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
-import { useForm, ValidationError } from '@formspree/react'
+import React, { useState } from 'react'
+import { useForm } from '@formspree/react'
 
 export const ContactForm = () => {
   const [state, handleSubmit] = useForm('moqbvadw')
+  const [hasName, setHasName] = useState<boolean>(false)
+  const [hasEmail, setHasEmail] = useState<boolean>(false)
+  const [hasMessage, setHasMessage] = useState<boolean>(false)
+
+  const hasEmptyFields: boolean = !(hasName && hasEmail && hasMessage)
+
+  const isDisabled = state.submitting || hasEmptyFields
+
+  const resetForm = () => {
+    setHasName(false)
+    setHasEmail(false)
+    setHasMessage(false)
+  }
 
   if (state.succeeded) {
     return <h2>Thanks for your message!</h2>
@@ -12,21 +25,40 @@ export const ContactForm = () => {
     <form onSubmit={handleSubmit}>
       <div className="field half first">
         <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" />
+        <input
+          type="text"
+          name="name"
+          id="name"
+          required
+          onChange={(e) => {
+            setHasName(e.target.value !== '')
+          }}
+        />
       </div>
       <div className="field half">
         <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
-        <ValidationError prefix="Email" field="email" errors={state.errors} />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          required
+          onChange={(e) => {
+            const isValidEmail = !(
+              e.target.validity.typeMismatch || e.target.validity.valueMissing
+            )
+            setHasEmail(isValidEmail)
+          }}
+        />
       </div>
       <div className="field">
         <label htmlFor="message">Message</label>
-        <textarea name="message" id="message" rows={4} required></textarea>
-        <ValidationError
-          prefix="Message"
-          field="message"
-          errors={state.errors}
-        />
+        <textarea
+          name="message"
+          id="message"
+          rows={4}
+          required
+          onChange={(e) => setHasMessage(e.target.value !== '')}
+        ></textarea>
       </div>
       <ul className="actions">
         <li>
@@ -34,11 +66,11 @@ export const ContactForm = () => {
             type="submit"
             value="Send Message"
             className="special"
-            disabled={state.submitting}
+            disabled={isDisabled}
           />
         </li>
         <li>
-          <input type="reset" value="Reset" />
+          <input type="reset" value="Reset" onClick={resetForm} />
         </li>
       </ul>
     </form>
